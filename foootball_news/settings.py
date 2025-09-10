e87a739c7011980509dcfe3641e50b200f8f8856
IkanBerkepalaLele <ehgojim@gmail.com>
2025-09-10 15:15:54 +0700

    ganti settings.py

--- foootball_news/settings.py
+++ foootball_news/settings.py
@@ -86,11 +86,11 @@ if PRODUCTION:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('ghozam.muliawan'),
-            'USER': os.getenv('ghozam.muliawan'),
-            'PASSWORD': os.getenv('IeooqsVM'),
-            'HOST': os.getenv('152.118.29.139'),
-            'PORT': os.getenv('5432'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
             'OPTIONS': {
                 'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
             }
